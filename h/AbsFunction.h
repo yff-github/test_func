bf7d626964737804498761b5409e3dade6e56fd3
2242182284@qq.com <2242182284@qq.com>
2021-07-30 21:46:07 +0800

    qqqq

--- h/AbsFunction.h
+++ h/AbsFunction.h
@@ -1,10 +1,12 @@
 #ifndef __AbsFunction__
 #define __AbsFunction__
+#include<string>
 class absfunction{
-    
-
-
-
+public:
+    absfunction(){}
+    virtual std::string Expression(void)=0;
+    virtual double operator()(double)=0; 
+    virtual int derivation(int)=0;
 };
 
 #endif
